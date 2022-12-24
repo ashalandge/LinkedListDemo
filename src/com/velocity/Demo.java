e7271eb1815294d2c72bb07cc3aba41b2d52d5fb
ashalandge <ashalandge54@gmail.com>
2022-12-24 23:08:30 +0530

    first commit : first code linkedlist

---
+++ src/com/velocity/Demo.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+import java.util.LinkedList;
+
+public class Demo {
+public static void main(String[] args) {
+	LinkedList list =new LinkedList();
+	list.add(1);
+	list.add(2);
+	list.add(3);
+	System.out.println(list);
+}
+}
